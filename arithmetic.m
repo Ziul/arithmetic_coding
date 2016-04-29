4e39ffea7dc6ee61f9ebb3798847be323dcefbf2
Luiz Oliveira <ziuloliveira@gmail.com>
2016-04-29 05:33:07 -0300

    Fixed some typo and minnor issues
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- arithmetic.m
+++ arithmetic.m
@@ -3,18 +3,20 @@
 clear classes;
 
 % Import module
+% You can import the file inside src folder or the package installed
 mod = py.importlib.import_module('arithmetic');
+
 
 % Some source and code example
 source_code = 'arithmetic';
-code = 0.0757451536000
+code = 0.0757451536000;
 
 % Setup the probabilities and the rage for each symbol
 probabilities = py.arithmetic.set_probability(source_code);
 symbols_with_range = py.arithmetic.set_range(probabilities);
 
 % Encoding
-result = py.arithmetic.encode(source_code, probabilities);
+encoded = py.arithmetic.encode(source_code, symbols_with_range)
 
 % Decoding
-result = py.arithmetic.encode(code, probabilities);
+decoded = py.arithmetic.decode(encoded, symbols_with_range)
